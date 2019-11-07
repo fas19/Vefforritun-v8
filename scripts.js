e7d46dff11c93ae891e251abb426ac3c98ee5a6b
fas19 <47185536+fas19@users.noreply.github.com>
2019-11-07 09:21:26 +0000

    annað repo

--- scripts.js
+++ scripts.js
@@ -9,10 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const text = (() => {
   let items;
+  let textarea;
+  let checked;
+  let deletee;
 
   function init(_form, _items) {
     items = _items;
+    textarea = _form.querySelector("input");
+    checked = _items.querySelector(".item__checkbox");
+    deletee = _items.querySelector(".item__button");
     _form.addEventListener('submit', formHandler);
+    checked.addEventListener('change', edit);
+    deletee.addEventListener('click',deleteItem);
+    
+   
 
     // TODO láta hluti í _items virka
   }
@@ -21,6 +31,8 @@ const text = (() => {
     e.preventDefault();
 
     console.log('halló heimur');
+    var div = el("li", "item", formHandler);
+    add(div);
   }
 
   // event handler fyrir það að klára færslu
@@ -29,6 +41,10 @@ const text = (() => {
 
   // event handler fyrir það að breyta færslu
   function edit(e) {
+    e.preventDefault();
+    items.classList.add("item--done");
+    console.log("Halló maður");
+    event.stopPropagation();
   }
 
   // event handler fyrir það að klára að breyta færslu
@@ -37,14 +53,40 @@ const text = (() => {
 
   // fall sem sér um að bæta við nýju item
   function add(value) {
+
+    items.appendChild(value);
+
   }
 
   // event handler til að eyða færslu
   function deleteItem(e) {
+    e.preventDefault();
+
+    console.log('halló heimur2');
   }
 
   // hjálparfall til að útbúa element
   function el(type, className, clickHandler) {
+    var divElement = document.createElement(type);
+    divElement.classList.add(className);
+    var inp = document.createElement("input");
+    inp.classList.add("item__checkbox");
+    inp.setAttribute("type","checkbox");
+    divElement.appendChild(inp);
+    var sp = document.createElement("span");
+    sp.classList.add("item__text");
+    var y = document.createTextNode(textarea.value);
+    sp.appendChild(y);
+    divElement.appendChild(sp);
+    var bt = document.createElement("button");
+    bt.classList.add("item__button");
+    var z = document.createTextNode("Eyða");
+    bt.appendChild(z);
+    divElement.appendChild(bt);
+
+    
+  
+    return divElement;
   }
 
   return {
